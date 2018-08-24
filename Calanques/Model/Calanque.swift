ba2acbb9d8ecced5838636aff3711b0f8ddae23b
Mars8.6 <mars8.6@hotmail.fr>
2018-08-24 11:36:09 +0200

    Ajout de la classe calanque

---
+++ Calanques/Model/Calanque.swift
@@ -0,0 +1,15 @@
+//
+//  Calanque.swift
+//  Calanques
+//
+//  Created by Meyer on 24/08/2018.
+//  Copyright © 2018 MarsMongars. All rights reserved.
+//
+
+import UIKit
+
+class Calanque
+
+{
+    
+}
