b007624219315684165f6380319e549d158649f9
Mars8.6 <mars8.6@hotmail.fr>
2018-08-24 11:51:54 +0200

    Modification de la classe Calanque et ajout de la classe CalanqueCollection

--- Calanques/Model/Calanque.swift
+++ Calanques/Model/Calanque.swift
@@ -7,9 +7,46 @@
 //
 
 import UIKit
+import CoreLocation
 
 class Calanque
 
 {
+    private var  _nom : String
+    private var _desc : String
+    private var _latitude : Double
+    private var _longitude : Double
+    private var _image : UIImage?
+    
+    var nom : String
+    {
+        return _nom
+    }
+    
+    var desc : String
+    {
+        return _desc
+    }
+    
+    var coordoones :CLLocationCoordinate2D
+    {
+        return CLLocationCoordinate2D(latitude: _latitude, longitude: _longitude)
+        
+    }
+    
+    var image : UIImage?
+    {
+        return _image
+    }
+    
+    init (nom : String, desc :String, image : UIImage?, latitude :Double , longitude : Double)
+    {
+        _nom = nom
+       _desc = desc
+       _image = image
+        _latitude = latitude
+        _longitude = longitude
+        
+    }
     
 }
